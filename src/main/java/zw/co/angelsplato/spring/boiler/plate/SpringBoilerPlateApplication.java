f8b5eb815490a1654974565000cb62098ff181f1
Nyabinde Nyasha Elvis <37302304+nyabindenyasha@users.noreply.github.com>
2021-02-26 11:33:56 +0200

    Initial commit

---
+++ src/main/java/zw/co/angelsplato/spring/boiler/plate/SpringBoilerPlateApplication.java
@@ -0,0 +1,13 @@
+package zw.co.angelsplato.spring.boiler.plate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoilerPlateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoilerPlateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zw/co/angelsplato/spring/boiler/plate/SpringBoilerPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.angelsplato.spring.boiler.plate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoilerPlateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
